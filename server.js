d0f9f01f178f920253019f3105214cb3ab8d9fe5
Paweł Kamyszek <hello@filar.co>
2023-10-14 22:27:56 +0200

    CD pipeline test

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello New World Really!");
+  res.send("This was deployed automatically!");
 });
 
 app.listen(port, () => {
