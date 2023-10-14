d1d3938e66365a7d74a3123223291cc75fb1cb08
Paweł Kamyszek <hello@filar.co>
2023-10-14 23:01:37 +0200

    Try2

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("This was deployed automatically now!");
+  res.send("This was deployed automatically now go!");
 });
 
 app.listen(port, () => {
