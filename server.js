b9b953a17c8e2ccf7f7b4e781ca9a3d71f09dc60
Paweł Kamyszek <hello@filar.co>
2023-10-14 23:09:51 +0200

    Try 3

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("This was deployed automatically now go!");
+  res.send("This was deployed automatically now go finally!");
 });
 
 app.listen(port, () => {
