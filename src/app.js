e6ba1d0ba648c01884d5b3004d1f453b9bf2c6c5
Paweł Kamyszek <hello@filar.co>
2023-10-15 21:44:05 +0200

    Setting up logging with Winston and Morgan. Adding axios. Testing external API calls

--- src/app.js
+++ src/app.js
@@ -1,17 +1,41 @@
-// const express = require("express");
-// const helmet = require("helmet");
 import express from "express";
 import helmet from "helmet";
+import axios from "axios";
+import logger from "./loggers/winston.js";
+import morganMiddleware from "./loggers/morgan.js";
 
 const app = express();
+
+/*
+  TODO: The logging Section.
+*/
+app.use(morganMiddleware);
 
 // Use Helmet!
 app.use(helmet());
 // Disable default X-Powered-By response header banner
 app.disable("x-powered-by");
 
+const printer = () => "ES6 in action";
+
 app.get("/", (req, res) => {
-  res.send("We got this!!!");
+  res.send(printer());
+  logger.info("Info log");
+});
+
+app.get("/crypto", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://api2.binance.com/api/v3/ticker/24hr"
+    );
+
+    const tickerPrice = response.data;
+
+    res.json(tickerPrice);
+  } catch (err) {
+    logger.error(err);
+    res.status(500).send("Internal server error");
+  }
 });
 
 export default app;
---
+++ src/loggers/winston.js
@@ -0,0 +1,16 @@
+import * as winston from "winston";
+// Winston settings
+const { combine, timestamp, json } = winston.format;
+
+const logger = winston.createLogger({
+  level: "http",
+  format: combine(
+    timestamp({
+      format: "YYYY-MM-DD hh:mm:ss.SSS A",
+    }),
+    json()
+  ),
+  transports: [new winston.transports.Console()],
+});
+
+export default logger;
