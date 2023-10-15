c264527b261498f8d9e2dd56f7d399518bd71830
Paweł Kamyszek <hello@filar.co>
2023-10-15 15:36:27 +0200

    Try that 5

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("This was deployed automatically now go finally gits 3!");
+  res.send("This was deployed automatically now go finally gits 6!");
 });
 
 app.listen(port, () => {
